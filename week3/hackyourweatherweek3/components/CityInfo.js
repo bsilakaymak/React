8c6242e3a1ce4f38f81b0d051231f6d7be70333a
Sila <basaksilakaymak@gmail.com>
2020-02-12 14:12:28 +0100

    changes after feedback

--- week3/hackyourweatherweek3/components/CityInfo.js
+++ week3/hackyourweatherweek3/components/CityInfo.js
@@ -5,23 +5,21 @@ import Details from "./Details";
 import RemoveButton from "./RemoveButton";
 
 const CityInfo = ({ cityWeather, remove }) => {
-        return cityWeather.map(item => {
-            return (
-              <div id="info">
-                <CityName item={item} />
-                <Description item={item} />
-                <Details
-                  item={item}
-                  kelvinToCelcius={kelvin => {
-                    return (kelvin - 273).toFixed(2);
-                  }}
-                />
-                <RemoveButton handleOnClick={remove} item={item} />
-              </div>
-            );
-          });
-    }
-    
+  return cityWeather.map((item, i) => {
+    return (
+      <div className="info" key={i}>
+        <CityName item={item} />
+        <Description item={item} />
+        <Details
+          item={item}
+          kelvinToCelcius={kelvin => {
+            return (kelvin - 273).toFixed(2);
+          }}
+        />
+        <RemoveButton handleOnClick={remove} item={item} />
+      </div>
+    );
+  });
+};
 
-;
 export default CityInfo;
